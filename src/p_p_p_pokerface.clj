4462c4a66ebae5a664b2d837504794b875264811
Paul Bacchus <paulbacchus@P2X-3YZ.local>
2015-03-18 18:07:26 +0000

    pokerface answers.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,28 +1,44 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
+  (let [[fst _] card]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      (get replacements fst))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (let [card-ranks (map rank hand)
+        card-frequencies (frequencies card-ranks)]
+    (boolean (some #(= 2 %) (vals card-frequencies)))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [card-ranks (map rank hand)
+        card-frequencies (frequencies card-ranks)]
+    (boolean (some #(= 3 %) (vals card-frequencies)))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [card-ranks (map rank hand)
+        card-frequencies (frequencies card-ranks)]
+    (boolean (some #(= 4 %) (vals card-frequencies)))))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [card-ranks (map rank hand)
+        card-frequencies (frequencies card-ranks)]
+    (or (four-of-a-kind? hand)
+        (=
+          (count (filter (fn [[_ v]] (= v 2)) card-frequencies))
+          2))))
 
 (defn straight? [hand]
   nil)
